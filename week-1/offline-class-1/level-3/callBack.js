ccc57625a6aae58c14d2bc276841301ab960779a
Aatif <aatifkhan2001@gmail.com>
2025-02-24 09:18:56 +0530

    Added callbacks

--- week-1/offline-class-1/level-3/callBack.js
+++ week-1/offline-class-1/level-3/callBack.js
@@ -1,4 +1,4 @@
-function arithmetic(a,b,type){ // In this function we are passing function as an argument
+function arithmetic(a,b,type){ 
     if(type=="sum"){
         return a+b; 
     }
@@ -7,3 +7,25 @@ function arithmetic(a,b,type){ // In this function we are passing function as an
     }
 }
 console.log(arithmetic(9,7,"sum"));
+
+
+function arithmetic1(a,b,type){ 
+    if(type=="sum"){
+        const value =sum(a,b);
+        return value; 
+
+    }
+    else if (type=="sub"){
+        const value =sub(a,b);
+        return value; 
+    }
+}
+
+function sum(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+const ans = arithmetic1(9,7,"sub");
+console.log(ans);

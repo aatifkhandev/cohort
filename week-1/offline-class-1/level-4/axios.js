635f5abc10a47dd134e9f97b86db2d04504879b3
Aatif <aatifkhan2001@gmail.com>
2025-03-02 12:45:42 +0530

    Added Axios Js

--- week-1/offline-class-1/level-4/axios.js
+++ week-1/offline-class-1/level-4/axios.js
@@ -1,21 +1,23 @@
 // Axios API - It is a library used to make HTTP requests to the server. It is a promise-based library and developer-friendly.
 // to install - npm install axios / use CDN in HTML
 const axios = require('axios');
-axios.get(`https://randomuser.me/api`)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log('Error:', error);
-    });
+// axios.get(`https://randomuser.me/api`)
+//     .then(response => {
+//         console.log(response.data);
+//     })
+//     .catch(error => {
+//         console.log('Error:', error);
+//     });
+
+
 
 async function fetchRandomUser1(){
     try{
-        const data =  await axios.get('https://randomuser.me/api')
+        const {data} =  await axios.get('https://randomuser.me/api')
         console.log(data);
         
     }catch(error){
-        console.error('Error fetching data:', error.message);
+        console.error('Error fetching data:', error.message||error);
     }
 }
 

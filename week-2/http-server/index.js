7e397e5ebeca66666928ae3a4858a6c2650ceb13
Aatif <aatifkhan2001@gmail.com>
2025-03-07 11:42:21 +0530

    Added http server

--- week-2/http-server/index.js
+++ week-2/http-server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const port = 3001;
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send("Hello World");
@@ -15,7 +18,7 @@ app.get("/route-handler",(req,res)=>{
 })
 
 app.post('/conversation',(req,res)=>{
-    console.log(req.headers);
+    console.log(req.body);
     
  res.json({
     msg:"2+2 = 4"

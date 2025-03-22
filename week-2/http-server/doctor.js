6d6e475dcbc3725ddaccea075353fa4d9ac6d161
Aatif <aatifkhan2001@gmail.com>
2025-03-22 16:19:19 +0530

    Added filter function in server js

--- week-2/http-server/doctor.js
+++ week-2/http-server/doctor.js
@@ -1,18 +1,42 @@
 const express = require('express')
 const app = express()
-const port = 3004;
-function sum(n){
-    let ans = 0
-    for(let i=1;i<=n;i++ ){
-        ans+=i;
-    }
-    return ans
-}
+const port = 3000;
+
+
+const users = [{
+    name : "john",
+    kidneys : [{
+        healthy : true
+    },]
+}]
+
 
 app.get('/',(req,res)=>{
-    const n = req.query.n
-    const ans = sum(n)
-    res.send("Hi your ans is :" + ans)
+    const johnKidneys = users[0].kidneys
+    const numberOfKidneys = johnKidneys.length
+  
+    
+    // const numberOfHealthyKidneys = 0
+    // for(let i=0;i<johnKidneys.length;i++){
+        //     if(johnKidneys[i].healthy){
+            //         numberOfHealthyKidneys = numberOfHealthyKidneys+1
+            //     }
+            // }
+            
+            
+             let numberOfHealthyKidneys = johnKidneys.filter((item)=>{
+                return item.healthy;
+
+             }).length
+             
+
+    const numberOfUnhealthyHealthyKidneys = numberOfKidneys - numberOfHealthyKidneys
+
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyHealthyKidneys
+    })
 })
 
 

c679ba2c56a644f6f6eada1122630f00c0018399
Aatif <aatifkhan2001@gmail.com>
2025-04-17 14:06:35 +0530

    Added server

--- week-2/http-server/server2.js
+++ week-2/http-server/server2.js
@@ -18,8 +18,53 @@ const users = [{
 app.use(express.json())
 
 
+function isThereAtLeastOneUnHealthyKidney(){
+  let AtLeastOneUnHealthyKidney = false
+  for(let i= 0;i<users[0].kidneys.length;i++){
+    if(!users[0].kidneys[i].healthy){
+      AtLeastOneUnHealthyKidney = true
+    }
+  }
+  return AtLeastOneUnHealthyKidney
+}
+   
 app.get('/',(req,res)=>{
   const abcKidneys = users[0].kidneys
-  const numberOfKidneys = users[0].kidneys.length
+  const numberOfKidneys = abcKidneys.length
+  let numberOfHealthyKidneys = 0
+  for(let i = 0 ; i<abcKidneys.length;i++){
+    if(abcKidneys[i].healthy){
+      numberOfHealthyKidneys=numberOfHealthyKidneys+1
+    }
+  }
+  const numberOfUnhealthyHealthyKidneys = numberOfKidneys-numberOfHealthyKidneys
+
+  res.json({
+    numberOfKidneys,
+    numberOfHealthyKidneys,
+    numberOfUnhealthyHealthyKidneys
+  })
+})
+
+app.post('/',(req,res)=>{
+const isHealthy = req.body.isHealthy;
+users[0].kidneys.push({
+  healthy:isHealthy
+})
+res.json({
+  msg : "isDone"
+})
+})
+
+app.put('/',(req,res)=>{
+  for(let i=0;i<users[0].kidneys.length;i++){
+    
+  }
+})
+
+app.listen(port,()=>{
+  console.log(`app listening on port ${port}`);
   
 })
+
+

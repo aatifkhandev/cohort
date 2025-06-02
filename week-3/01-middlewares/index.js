52cf45c379b07b76ae8877a5711bd93365600df6
Aatif <aatifkhan2001@gmail.com>
2025-06-02 14:32:51 +0530

    Feat : Added MiddleWare's

--- week-3/01-middlewares/index.js
+++ week-3/01-middlewares/index.js
@@ -6,30 +6,70 @@
 const express = require('express')
 const app = express()
 
-app.get('/health-checkup',(req,res)=>{
-    const kidneyId = req.query.kidneyId;
-    const userName  = req.headers.userName;
-    const passwords = req.headers.passwords;
-
-    if(userName!="aatif" && passwords!="12345"){
+function userMiddleWare(req,res,next){
+    const userName = req.headers.username;
+    const password = req.headers.password;
+    if(userName!="aatif" && password!="123"){
         res.status(403).json({
-            msg:"user doesn't exist"
-        });
-        return
+            msg:"Incorrect Inputs"
+        })
+    }else{
+        next();
     }
-    if(kidneyId!=1 && kidneyId!=2){
-        res.status(411).json({
-            msg:"wrong inputs"
-        });
-        return;
-    }
+}
 
-res.send({
-    "heart":"healthy"
+function kidneyMiddleWare(req,res,next){
+       const kidneyId = parseInt(req.query.kidneyId); 
+    if(kidneyId!=1 && kidneyId!=2){
+         res.status(403).json({
+            msg:"Incorrect Inputs"
+        })
+    }else{
+        next()
+    }
+}
+
+app.get('/health-checkup',userMiddleWare,kidneyMiddleWare,(req,res)=>{
+    res.send("your kidney is healthy")
 })
+
+app.get('/kidney-check',userMiddleWare,kidneyMiddleWare,(req,res)=>{
+    res.send("your kidney is healthy")
 })
+
+app.get('/heart-checkup',userMiddleWare,(req,res)=>{
+    res.send("your kidney is healthy")
+})
+
+
+
 
 app.listen(3000,()=>{
     console.log("starting on 3000");
     
 })
+
+
+//This code doesn't use middle wares
+// app.get('/health-checkup',(req,res)=>{
+//     const kidneyId = req.query.kidneyId;
+//     const userName  = req.headers.userName;
+//     const passwords = req.headers.passwords;
+
+//     if(userName!="aatif" || passwords!="12345"){
+//         res.status(403).json({
+//             msg:"user doesn't exist"
+//         });
+//         return
+//     }
+//     if(kidneyId!=1 && kidneyId!=2){
+//         res.status(411).json({
+//             msg:"wrong inputs"
+//         });
+//         return;
+//     }
+
+// res.send({
+//     "heart":"healthy"
+// })
+// })

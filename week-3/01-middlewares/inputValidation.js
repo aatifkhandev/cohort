9113c0d389ac5f7c0825da0f12f004af90519f5a
Aatif <aatifkhan2001@gmail.com>
2025-06-02 19:44:41 +0530

    Feat : Added input validation

--- week-3/01-middlewares/inputValidation.js
+++ week-3/01-middlewares/inputValidation.js
@@ -11,9 +11,16 @@ app.use(express.json())
 app.post('/health-checkup',(req,res)=>{
  const kidneys = req.body.kidneys;
  const response  = schema.safeParse(kidneys)
+ if(!response.success){
+    res.status(411).json({
+     msg : "input is valid"
+    })
+ }else{
  res.send({
 response
  })
+ }
+
 })
 
 app.listen(4001,()=>{

6caf17d60f26457ac359b01a0864796e34a09807
Aatif <aatifkhan2001@gmail.com>
2025-06-22 19:54:01 +0530

    Feat : Added practice file

---
+++ week-2/00-callBack-js/practice.js
@@ -0,0 +1,14 @@
+
+function cube(n){
+    return n*n*n
+}
+function square(n){
+    return n*n
+}
+
+function sumOfSomething(a,b,square){
+   return square(a)+square(b)
+}
+
+//  console.log(sumOfSomething(1,2,square))
+

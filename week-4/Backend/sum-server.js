655ebc5cb769cb26654c58f35c0bef8d35d6046b
Aatif <aatifkhan2001@gmail.com>
2025-07-30 13:33:08 +0530

    Fixed : Backend Server

--- week-4/Backend/sum-server.js
+++ week-4/Backend/sum-server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
 
+app.use(cors())
 
 app.get('/sum',(req,res)=>{
     const a = parseInt(req.query.a)

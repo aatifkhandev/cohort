69c60bd98b10171b6eaf126091f1fde20606c834
Aatif <aatifkhan2001@gmail.com>
2025-08-09 23:00:44 +0530

    Feat : Added Advance middleWares

--- week-3/05-assesments/middleWare-Advance-Task.js
+++ week-3/05-assesments/middleWare-Advance-Task.js
@@ -7,6 +7,34 @@ let tasks = []
 app.use(express.json())
 
 
+const requests = {}
+app.use((req,res,next)=>{  // 1-> rate - limiter
+  const ip = (req.ip);
+  const now = Date.now()
+
+    if (!requests[ip]) {
+    // First time seeing this IP
+    requests[ip] = { count: 1, firstRequestTime: now };
+    return next();
+  }
+
+    const timeSinceFirstRequest = now - requests[ip].firstRequestTime;
+
+      if (timeSinceFirstRequest < 60 * 1000) { // within 1 minute
+    requests[ip].count++;
+
+     if (requests[ip].count > 5) {
+      return res.status(429).json({ msg: "Too many requests. Try again later." });
+    }
+      } else {
+    // Reset counter after 1 minute
+    requests[ip] = { count: 1, firstRequestTime: now };
+  }
+  next();
+
+
+}) 
+
 
 app.get('/tasks',(req,res)=>{
  res.send(tasks)    

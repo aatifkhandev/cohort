2a6df3a62c590a94943dcad21a37c6cbf8da2d7c
Aatif <aatifkhan2001@gmail.com>
2025-08-11 21:32:57 +0530

    Fix :Authentication

--- week-3/02-jwt/jsonWebEx-1.js
+++ week-3/02-jwt/jsonWebEx-1.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
-const SECRET_KEY = "my_secret_key";
+const SECRET_KEY = "12345";
 
-const user = {id:1,username:"aatif"};
+const user = {id:1,username:"aatif",role:"foo"};
 const token = jwt.sign(user,SECRET_KEY,{expiresIn:'1hr'})
 
 console.log("Generated token",token);
